e0bc31ee27e4ffeffc10ef3fcc274fd8343beffa
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2020-10-03 16:37:46 +0200

    initial app setup with redux and router

---
+++ src/reducers/movies.js
@@ -0,0 +1,11 @@
+const initialState = {
+  isFetching: false,
+  records: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
