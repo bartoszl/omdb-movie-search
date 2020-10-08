8a6dc3aa0873fc4bbad00e982706555c4fda329e
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2020-10-08 22:04:34 +0200

    Updates

---
+++ precache-manifest.0e3191e798b0a4d57f5b2f477b21fb4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7346911e500d4259b92ede010ac72ba7",
+    "url": "/omdb-movie-search/index.html"
+  },
+  {
+    "revision": "92d9787111ca719cba3b",
+    "url": "/omdb-movie-search/static/js/2.179ef3e1.chunk.js"
+  },
+  {
+    "revision": "a1dd73de632d3c9cf39d7872e4c706cb",
+    "url": "/omdb-movie-search/static/js/2.179ef3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80004a0829f039001e3",
+    "url": "/omdb-movie-search/static/js/main.be38d56f.chunk.js"
+  },
+  {
+    "revision": "cb936d5378fbd590ea4b",
+    "url": "/omdb-movie-search/static/js/runtime-main.629d5172.js"
+  },
+  {
+    "revision": "7c4c06c1aa2cf344c7f23419cad8a592",
+    "url": "/omdb-movie-search/static/media/movie-background.7c4c06c1.jpg"
+  }
+]);
