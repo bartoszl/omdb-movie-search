21c12767c9a6a511a8e79ea4b3df1d72d437f34a
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2020-10-09 14:08:57 +0200

    vader background for search

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 import { BACKGROUND_GREY } from '../constants/colors';
+import bg from '../assets/search-bg.jpg';
 
 const SearchContainer = styled.div`
   width: 100vw;
   height: ${({ collapsed }) => (collapsed ? '15vh' : '100vh')};
+  background-image: ${({ collapsed }) => (!collapsed ? `url(${bg})` : 'none')};
   background-color: ${BACKGROUND_GREY};
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  transition: height 0.75s ease-out;
+  transition: all 0.75s ease-out;
   position: fixed;
   top: 0;
   left: 0;
