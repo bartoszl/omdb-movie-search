390e8eb79f212e41886956e4e958c17a110947f2
Bartosz Lewandowski <barteklewandowski94@gmail.com>
2020-10-11 19:53:34 +0200

    fix center component and add readme.md

--- src/pages/IndexPage/IndexPage.js
+++ src/pages/IndexPage/IndexPage.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
 import useFilters from '../../hooks/useFilters';
 import {
   Movie, MoviesList, CountDisplay, Loader, LoadMoreButton, Center,
